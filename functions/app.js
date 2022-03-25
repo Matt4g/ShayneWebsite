0353107169a5c66c68a663aa9c57c00b2505f017
Matt Gladura <75256340+Matt4g@users.noreply.github.com>
2022-03-25 11:40:15 -0400

    I dont know whats different

--- functions/app.js
+++ functions/app.js
@@ -1,1 +1,7 @@
-console.log(document.querySelector(".fade-in-question"));
+let varA = document.querySelector(".fade-in-question");
+if(varA == null){
+    console.log('FUCK')
+}
+else{
+    console.log("lets go")
+}
